bb70829bcc92186d380cf9663c4ce17a005c5a3c
Leo Schindler-Gerendasi <dr.wario07@gmail.com>
2025-09-29 18:44:49 -0400

    Added some really square confetti
    This confetti emits when a player reaches the end of the level, and appears above that player's head. This is mostly there for funsies

--- pullItTogether/Scripts/MiscScripts/TransitionArea.cs
+++ pullItTogether/Scripts/MiscScripts/TransitionArea.cs
@@ -7,11 +7,20 @@ public partial class TransitionArea : Area3D
 {
     [Export]
     private Label3D label;
+    [Export]
+    private GpuParticles3D pinkConfetti;
+    [Export]
+    private GpuParticles3D yellowConfetti;
+    [Export]
+    private GpuParticles3D cyanConfetti;
 
     private void OnBodyEntered(Node3D node)
     {
+        // TEMPORARY CODE: Not all transition areas should change Label3D text or summon confetti
         label.Text = "You did it :)";
         SetDeferred("monitoring", false);
+        pinkConfetti.Position = yellowConfetti.Position = cyanConfetti.Position = new Vector3(node.Position.X, 11, -60);
+        pinkConfetti.Emitting = yellowConfetti.Emitting = cyanConfetti.Emitting = true;
         ResetAfterSeconds(3); // Warning isn't important because this is super temporary code
     }
 
