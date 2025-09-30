6c335992a455092f24a18f40403f12c98f893e39
jack-ryan-keegan <jrk8986@g.rit.edu>
2025-09-30 16:23:41 -0400

    Wheels & Things
    - Added wheels and finished "suspension"

--- pullItTogether/Scripts/WagonScripts/RaycastWagon.gd
+++ pullItTogether/Scripts/WagonScripts/RaycastWagon.gd
@@ -4,6 +4,7 @@ extends RigidBody3D
 @export var spring_strength := 100.0
 @export var spring_dampen := 2.0
 @export var rest_distance := 0.5
+@export var wheel_radius := 0.7
 
 func _physics_process(delta: float) -> void:
 	for wheel in wheels:
@@ -16,16 +17,22 @@ func _get_point_velocity(point: Vector3) -> Vector3:
 
 func _do_single_wheel_suspension(suspension_ray: RayCast3D) -> void:
 	if suspension_ray.is_colliding():
+		suspension_ray.target_position.y = -(rest_distance + wheel_radius)
 		var contact := suspension_ray.get_collision_point()
 		var spring_up_dir := suspension_ray.global_transform.basis.y
-		var spring_len := suspension_ray.global_position.distance_to(contact)
+		var spring_len := suspension_ray.global_position.distance_to(contact) - wheel_radius
 		var offset := rest_distance - spring_len
+		
+		suspension_ray.get_node("Wheel").position.y = -spring_len
 		
 		var spring_force := spring_strength * offset
 		
+		var world_vel := _get_point_velocity(contact)
+		var relative_vel := spring_up_dir.dot(world_vel)
+		var spring_damp_force := spring_dampen * relative_vel
 		
-		
-		var force_vector := spring_force * spring_up_dir
+		var force_vector := (spring_force - spring_damp_force) * spring_up_dir
 		
 		var force_pos_offset := contact - global_position
 		apply_force(force_vector, force_pos_offset)
+		
