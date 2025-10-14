0ff9179b4141bb63de0f57ada17f9ce80fb19daa
jack-ryan-keegan <jrk8986@g.rit.edu>
2025-10-14 11:41:50 -0400

    Added comments/removed unused code

--- pullItTogether/Scripts/Wagon/Wagon.cs
+++ pullItTogether/Scripts/Wagon/Wagon.cs
@@ -3,14 +3,18 @@ using System;
 
 public partial class Wagon : RigidBody3D
 {
+    //these values are multiplied with the wagon's linear velocity and angular velocity respectively
+    //linearXMultiplier reduces the ease at which the wagon can be pulled sideways
+    //angularYMultiplier reduces the ease at which the wagon can be turned around the y axis (left and right)
+    //this makes the wagon move more like it has wheels
     [Export] float linearXMultiplier = 0.25f;
     [Export] float angularYMultiplier = 0.75f;
 
     public override void _IntegrateForces(PhysicsDirectBodyState3D state)
     {
+        //multiplies the liner and angular velocities of the wagon by the specified values
         state.LinearVelocity *= new Vector3(linearXMultiplier, 1f, 1f);
         state.AngularVelocity *= new Vector3(1f, angularYMultiplier, 1f);
-        //GD.Print(state.AngularVelocity);
         base._IntegrateForces(state);
     }
 
